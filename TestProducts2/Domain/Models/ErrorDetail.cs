00fe1c9a5e57a02d04fbd1106c5888d2daaf9625
eytienne87 <etienne_tessier100@hotmail.ca>
2022-04-28 09:54:38 -0400

    created errordetail model

---
+++ TestProducts2/Domain/Models/ErrorDetail.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace Domain.Models
+{
+    public class ErrorDetail
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; } = string.Empty;
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
